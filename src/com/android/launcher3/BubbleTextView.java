7b3bfba5052dabd0d1156c46140c840de1d09305
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-11 09:07:49 +0800

    [bugfix] fixing hidden app labels issue
    https://t.me/AxionOS_android_chat/9015
    
    Change-Id: I78a3da1c965814cd86b92e95046e855953c90719
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -343,7 +343,12 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             defaultIconSize = mDeviceProfile.iconSizePx;
             mShouldShowLabel = SHOW_DESKTOP_LABELS.get(context);
         }
-
+        
+        if (mDisplay == DISPLAY_WORKSPACE && !mShouldShowLabel) {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 0);
+            setCompoundDrawablePadding(0);
+            setCenterVertically(true);
+        }
 
         mIconSize = a.getDimensionPixelSize(R.styleable.BubbleTextView_iconSizeOverride,
                 defaultIconSize);
