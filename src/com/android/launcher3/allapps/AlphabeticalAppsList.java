4427c8c2221a21f1545410c6633bd17882fec237
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-04 12:52:16 +0800

    [bugfix] oem sorting fix
    Change-Id: Ib001f04e7b8dede243a8c818cc6e71805b6173f7

--- src/com/android/launcher3/allapps/AlphabeticalAppsList.java
+++ src/com/android/launcher3/allapps/AlphabeticalAppsList.java
@@ -47,6 +47,7 @@ import com.android.launcher3.views.ActivityContext;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
@@ -88,7 +89,6 @@ public class AlphabeticalAppsList implements AllAppsStore.OnUpdateListener {
         }
     }
 
-
     private final ActivityContext mActivityContext;
 
     // The set of apps from the system
@@ -113,7 +113,6 @@ public class AlphabeticalAppsList implements AllAppsStore.OnUpdateListener {
     private int mNumAppsPerRowAllApps;
     private int mNumAppRowsInAdapter;
     private Predicate<ItemInfo> mItemFilter;
-    private final boolean mSortSections;
 
     public AlphabeticalAppsList(ActivityContext activityContext, @Nullable AllAppsStore appsStore,
             WorkProfileManager workProfileManager, PrivateProfileManager privateProfileManager) {
@@ -137,7 +136,6 @@ public class AlphabeticalAppsList implements AllAppsStore.OnUpdateListener {
         mPrivateProfileDividerBadge.setSpan(new ImageSpan(context,
                         R.drawable.ic_private_profile_divider_badge, ImageSpan.ALIGN_CENTER),
                 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        mSortSections = context.getResources().getBoolean(R.bool.config_appsListSortSections);
     }
 
     /** Set the number of apps per row when device profile changes. */
@@ -261,7 +259,13 @@ public class AlphabeticalAppsList implements AllAppsStore.OnUpdateListener {
 
         // As a special case for some languages (currently only Simplified Chinese), we may need to
         // coalesce sections
-        if (mSortSections) {
+        Locale curLocale = mActivityContext.getResources().getConfiguration().locale;
+        Locale normalizedLocale = new Locale.Builder()
+                .setLanguage(curLocale.getLanguage())
+                .setRegion(curLocale.getCountry())
+                .build();
+        final boolean sortSections = normalizedLocale.equals(Locale.SIMPLIFIED_CHINESE);
+        if (sortSections) {
             // Compute the section headers. We use a TreeMap with the section name comparator to
             // ensure that the sections are ordered when we iterate over it later
             appSteam = appSteam.collect(Collectors.groupingBy(
