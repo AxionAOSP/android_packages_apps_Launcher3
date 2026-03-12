6222178900635078d9585722c7cfd48b55ad39a3
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-12 21:02:57 +0800

    [bugfix] fixing workspace hidden app labels issue
    https://t.me/AxionOS_android_chat/9015
    
    Change-Id: I78a3da1c965814cd86b92e95046e855953c90719
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -325,6 +325,11 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             mShouldShowLabel = SHOW_DESKTOP_LABELS.get(context);
         }
 
+        if (mDisplay == DISPLAY_WORKSPACE && !mShouldShowLabel) {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 0);
+            setCompoundDrawablePadding(0);
+            setCenterVertically(true);
+        }
 
         mIconSize = a.getDimensionPixelSize(R.styleable.BubbleTextView_iconSizeOverride,
                 defaultIconSize);
