8bf679a8abc7bc2c9af5c383bc4eb315a0c85f05
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-06 21:39:31 +0800

    Revert "[ax_perf] speeding up app open/exit ainmations"
    This reverts commit 06a517c5bb0dea4134419a7d83fa1153567b46b0.

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -210,7 +210,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
      */
     public static final int STATUS_BAR_TRANSITION_PRE_DELAY = 96;
 
-    public static final long APP_LAUNCH_DURATION = 350;
+    public static final long APP_LAUNCH_DURATION = 500;
 
     private static final long APP_LAUNCH_ALPHA_DURATION = 50;
     private static final long APP_LAUNCH_ALPHA_START_DELAY = 25;
@@ -226,7 +226,7 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
 
     public static final int RECENTS_LAUNCH_DURATION = 336;
     private static final int LAUNCHER_RESUME_START_DELAY = 100;
-    private static final int CLOSING_TRANSITION_DURATION_MS = 200;
+    private static final int CLOSING_TRANSITION_DURATION_MS = 250;
     public static final int SPLIT_LAUNCH_DURATION = 370;
     public static final int SPLIT_DIVIDER_ANIM_DURATION = 100;
 
