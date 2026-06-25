b9829d81973bace296b65471af11356e3037c9ec
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-26 06:25:07 +0800

    [bugfix] fixing hidden navigation hint freeze bug

--- quickstep/src/com/android/launcher3/taskbar/TaskbarInteractor.kt
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarInteractor.kt
@@ -188,16 +188,11 @@ class TaskbarInteractor(private val taskbarUIController: TaskbarUIController) {
     @AnyThread
     fun postOnRootViewDraw(callback: Runnable, callbackExecutor: Executor): Boolean {
         val rootView = taskbarUIController.rootView
-        return if (rootView != null) {
-            if (rootView.height == 0) {
-                // When view height is 0, we don't have to wait for anything.
-                false
-            } else {
-                executor.execute {
-                    ViewUtils.postFrameDrawn(rootView) { callbackExecutor.execute(callback) }
-                }
-                true
+        return if (rootView != null && rootView.height > 0) {
+            executor.execute {
+                ViewUtils.postFrameDrawn(rootView) { callbackExecutor.execute(callback) }
             }
+            true
         } else {
             false
         }
