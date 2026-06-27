3d009553164e5f7a2ab0d7cf68ac2080a1fa7be2
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-27 19:39:16 +0800

    [feat] add app lock launcher hooks

--- quickstep/src/com/android/quickstep/RecentTasksList.java
+++ quickstep/src/com/android/quickstep/RecentTasksList.java
@@ -42,6 +42,7 @@ import androidx.annotation.VisibleForTesting;
 import com.android.launcher3.statehandlers.DesktopVisibilityController;
 import com.android.launcher3.util.DaggerSingletonTracker;
 import com.android.launcher3.util.LooperExecutor;
+import com.android.quickstep.util.AxSandboxState;
 import com.android.quickstep.util.DesktopTask;
 import com.android.quickstep.util.ExternalDisplaysKt;
 import com.android.quickstep.util.GroupTask;
@@ -82,6 +83,7 @@ public class RecentTasksList {
     private final VirtualDeviceManager mVirtualDeviceManager;
     private final LooperExecutor mMainThreadExecutor;
     private final SystemUiProxy mSysUiProxy;
+    private final @Nullable AxSandboxState mSandboxState;
 
     // The list change id, increments as the task list changes in the system
     private int mChangeId;
@@ -119,12 +121,23 @@ public class RecentTasksList {
             SystemUiProxy sysUiProxy,
             TopTaskTracker topTaskTracker,
             DaggerSingletonTracker tracker) {
+        this(context, mainThreadExecutor, keyguardManager, virtualDeviceManager, sysUiProxy, null,
+                topTaskTracker, tracker);
+    }
+
+    public RecentTasksList(Context context, LooperExecutor mainThreadExecutor,
+            KeyguardManager keyguardManager, VirtualDeviceManager virtualDeviceManager,
+            SystemUiProxy sysUiProxy,
+            AxSandboxState sandboxState,
+            TopTaskTracker topTaskTracker,
+            DaggerSingletonTracker tracker) {
         mContext = context;
         mMainThreadExecutor = mainThreadExecutor;
         mKeyguardManager = keyguardManager;
         mVirtualDeviceManager = virtualDeviceManager;
         mChangeId = 1;
         mSysUiProxy = sysUiProxy;
+        mSandboxState = sandboxState;
         final IRecentTasksListener recentTasksListener = new IRecentTasksListener.Stub() {
             @Override
             public void onRecentTasksChanged() throws RemoteException {
@@ -176,6 +189,11 @@ public class RecentTasksList {
         mSysUiProxy.registerRecentTasksListener(recentTasksListener);
         tracker.addCloseable(
                 () -> mSysUiProxy.unregisterRecentTasksListener(recentTasksListener));
+        if (mSandboxState != null) {
+            mSandboxState.addChangeListener(this::onRecentTasksChanged);
+            tracker.addCloseable(
+                    () -> mSandboxState.removeChangeListener(this::onRecentTasksChanged));
+        }
 
         // We may receive onRunningTaskAppeared events later for tasks which have already been
         // included in the list returned by mSysUiProxy.getRunningTasks(), or may receive
@@ -435,13 +453,13 @@ public class RecentTasksList {
                 final TaskInfo taskInfo1 = rawTask.getBaseGroupedTask().getTaskInfo1();
                 final Task.TaskKey task1Key = createTaskKey(taskInfo1);
                 final Task task1 = Task.from(task1Key, taskInfo1,
-                        tmpLockedUsers.get(task1Key.userId) /* isLocked */);
+                        isAppLocked(task1Key, tmpLockedUsers) /* isLocked */);
 
                 if (rawTask.isBaseType(TYPE_SPLIT)) {
                     final TaskInfo taskInfo2 = rawTask.getBaseGroupedTask().getTaskInfo2();
                     final Task.TaskKey task2Key = createTaskKey(taskInfo2);
                     final Task task2 = Task.from(task2Key, taskInfo2,
-                            tmpLockedUsers.get(task2Key.userId) /* isLocked */);
+                            isAppLocked(task2Key, tmpLockedUsers) /* isLocked */);
                     allTasks.add(new SplitTask(task1, task2,
                             rawTask.getBaseGroupedTask().getSplitBounds()));
                 } else {
@@ -454,7 +472,7 @@ public class RecentTasksList {
                 Task task1 = loadKeysOnly
                         ? new Task(task1Key)
                         : Task.from(task1Key, taskInfo1,
-                                tmpLockedUsers.get(task1Key.userId) /* isLocked */);
+                                isAppLocked(task1Key, tmpLockedUsers) /* isLocked */);
                 Task task2 = null;
                 if (taskInfo2 != null) {
                     // Is split task
@@ -462,7 +480,7 @@ public class RecentTasksList {
                     task2 = loadKeysOnly
                             ? new Task(task2Key)
                             : Task.from(task2Key, taskInfo2,
-                                    tmpLockedUsers.get(task2Key.userId) /* isLocked */);
+                                    isAppLocked(task2Key, tmpLockedUsers) /* isLocked */);
                 } else {
                     // Is fullscreen task
                     if (isFirstVisibleTaskFound) {
@@ -488,6 +506,11 @@ public class RecentTasksList {
         }
 
         return allTasks;
+    }
+
+    private boolean isAppLocked(Task.TaskKey key, SparseBooleanArray users) {
+        return (mSandboxState != null && mSandboxState.hasAppLock(key.getPackageName()))
+                || (users != null && users.get(key.userId));
     }
 
     private Task createTask(TaskInfo taskInfo, Set<Integer> minimizedTaskIds) {
