322f81faf7ec16590ee7b270272d9019ec7ccb5a
Andy Wickham <awickham@google.com>
2026-07-08 09:42:04 +0800

    Update blur style for more things.
     - Quick Launch highlight (focusHighlight)
     - Drag bar at top of All Apps
     - Divider inside Private Space
    
    Demo (enabling blur via adb in the middle):
    https://drive.google.com/file/d/1MjYn1c1mz3m1llmJ-J6kLHUa1j3biv_H/view?usp=drive_link&resourcekey=0-6d1yf_4zDL0W9nTYe6HLNQ
    
    Fix: 413143411
    Test: Manual
    Flag: com.android.launcher3.all_apps_blur

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -532,9 +532,8 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         if (shouldRecreate) {
             mWallpaperThemeManager.recreateToUpdateTheme();
         } else if (Flags.allAppsBlur()) {
-            // For all apps, we only need to update the scrim, which draws the panel. But if the
-            // activity was recreated above, this is unnecessary.
-            getAppsView().invalidateHeader();
+            getAppsView().getContext().getTheme().applyStyle(getAllAppsBlurStyleResId(), true);
+            getAppsView().onThemeChanged();
         }
     }
 
